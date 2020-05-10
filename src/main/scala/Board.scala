1aadb239be1d7a0c9ad75876111fe0d83552a28e
Johan Särnbratt <johan@minna.tech>
2020-05-10 09:48:27 +0200

    Remove hashmap

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -1,51 +1,44 @@
-
-import scala.collection.mutable
-import scala.collection.immutable
+import scala.collection.immutable.Seq
 
 object Boardie {
   def newBoard(size: Int): Board = {
-    val board: immutable.Seq[immutable.Seq[Boolean]] = immutable.Seq.range(1, size+1)
+    val board: Seq[Seq[Boolean]] = Seq.range(1, size+1)
       .map(
         e =>
-          immutable.Seq.range(0,e)
+          Seq.range(0,e)
             .map((_: Int) => true)
       )
-    new Board(size, board, immutable.Seq.empty)
+    new Board(size, board, Seq.empty)
   }
 }
 
-class Board(size: Int, val board: immutable.Seq[immutable.Seq[Boolean]], val moves: immutable.Seq[Move]) {
+class Board(size: Int, val board: Seq[Seq[Boolean]], val moves: Seq[Move], oldPossibleMoves: Option[Seq[Move]] = None) {
+  lazy val hash = board.hashCode()
 
-  def validateMove(yxyx: Move, printIt: Boolean = false): Boolean = {
-    validateMove(yxyx.from.y, yxyx.from.x, yxyx.to.y, yxyx.to.x, printIt)
-  }
-  def validateMove(y1: Int, x1: Int, y2: Int, x2: Int, printIt: Boolean): Boolean = {
-    val dy = Math.abs(y2-y1)
-    val my = (y2+y1)/2
-    val dx = Math.abs(x2-x1)
-    val mx = (x2+x1)/2
+  def validateMove(move: Move, printIt: Boolean = false): Boolean = {
+    val dy = Math.abs(move.to.y-move.from.y)
+    val my = (move.to.y+move.from.y)/2
+    val dx = Math.abs(move.to.x-move.from.x)
+    val mx = (move.to.x+move.from.x)/2
     val valid = (dx == 0 || dx == 2) && (dy == 0 || dy == 2) &&
-      x1<=y1 && x2<=y2 &&
-      0<=y1 && 0<=x1 && 0<=y2 && 0<=x2 &&
-      y1<size && y2<size &&
-      board(y1)(x1) && board(my)(mx) && !board(y2)(x2)
-    if (printIt) println(s"($y1, $x1, $y2, $x2) is allowed: $valid")
+      move.from.x<=move.from.y && move.to.x<=move.to.y &&
+      0<=move.from.y && 0<=move.from.x && 0<=move.to.y && 0<=move.to.x &&
+      move.from.y<size && move.to.y<size &&
+      board(move.from.y)(move.from.x) && board(my)(mx) && !board(move.to.y)(move.to.x)
+    if (printIt) println(s"$move is allowed: $valid")
     valid
   }
-  def move(yxyx: Move): Board = {
-    move(yxyx.from.y, yxyx.from.x, yxyx.to.y, yxyx.to.x)
-  }
-  def move(y1: Int, x1: Int, y2: Int, x2: Int): Board = {
-    val my = (y2+y1)/2
-    val mx = (x2+x1)/2
-    val by1 = board.updated(y1, board(y1).updated(x1, false))
+  def move(move: Move): Board = {
+    val my = (move.from.y+move.to.y)/2
+    val mx = (move.from.x+move.to.x)/2
+    val by1 = board.updated(move.from.y, board(move.from.y).updated(move.from.x, false))
     val bmy = by1.updated(my, by1(my).updated(mx, false))
-    val by2 = bmy.updated(y2, bmy(y2).updated(x2, true))
-    new Board(size, by2, moves :+ Move(Pos(y1, x1), Pos(y2, x2)))
+    val by2 = bmy.updated(move.to.y, bmy(move.to.y).updated(move.to.x, true))
+    new Board(size, by2, moves :+ move)
   }
   def printBoard(): Unit = {
     board.foreach(
-      (row: immutable.Seq[Boolean]) => {
+      (row: Seq[Boolean]) => {
         print(" "*(size-row.length))
         row.foreach(value => if (value) print("X ") else print("' "))
         println("")
@@ -56,28 +49,41 @@ class Board(size: Int, val board: immutable.Seq[immutable.Seq[Boolean]], val mov
     println(s"Making initial move $p on size $size")
     new Board(size, board.updated(p.y, board(p.y).updated(p.x, false)), moves :+ Move(Pos(p.y, p.x), Pos(0, 0)))
   }
-  /*def copy(): Board = {
-    val newBoard = new Board(size)
-    newBoard.board = board.clone()
-    newBoard.moves = moves.clone()
-    newBoard
-  }*/
-  val allPositions: immutable.Seq[Pos] = {
-    immutable.Seq.range(0, size)
+  lazy val allPositions: Seq[Pos] = {
+    Seq.range(0, size)
       .flatMap(
         y =>
-          immutable.Seq.range(0, y+1)
+          Seq.range(0, y+1)
             .map(x => Pos(y, x))
       )
   }
-  val directions = immutable.Seq((2,2),(2,0),(0,2),(-2,-2),(-2,0),(0,-2))
-  lazy val possibleMoves: immutable.Seq[Move] = {
-    val o: immutable.Seq[Move] = allPositions.flatMap(yx =>
-      directions.map(dyx =>
-        Move(yx, Pos(yx.y+dyx._1, yx.x+dyx._2))
-      )
-    )
-    o.filter(xxx => validateMove(xxx))
+  private val directions = Seq(Pos(2,2),Pos(2,0),Pos(0,2),Pos(-2,-2),Pos(-2,0),Pos(0,-2))
+  lazy val possibleMoves: Seq[Move] = {
+    oldPossibleMoves match {
+      case None =>
+        allPositions
+          .flatMap(yx =>
+            directions.map(d =>
+              Move(yx, Pos(yx.y+d.y, yx.x+d.x))
+            )
+          ).filter(xxx => validateMove(xxx))
+      case Some(possibleMoves) =>
+        val lastMove = moves.last
+        val lastJumped = Pos((lastMove.from.y+lastMove.to.y)/2, (lastMove.from.x+lastMove.to.x)/2)
+        val keepFromOld = possibleMoves.filter((move: Move) => {
+          val jump = Pos((move.from.y + move.to.y) / 2, (move.from.x + move.to.x) / 2)
+          if (move.from == lastMove.from) false
+          else if (move.from == lastJumped) false
+          else if (jump == lastMove.from) false
+          else if (jump == lastJumped) false
+          else if (move.to == lastMove.to) false
+          else true
+        })
+        val newMoves = directions.map(d =>
+          Move(lastMove.to, Pos(lastMove.to.y+d.y, lastMove.to.x+d.x))
+        ).filter(xxx => validateMove(xxx))
+        keepFromOld ++ newMoves
+    }
   }
   def isSolved(): Boolean = {
     board.flatten.count(x => x) == 1
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,32 +1,41 @@
+import scala.collection.mutable
 
 object Main extends App {
+  val size = 6
+  val bigNumber = size*size+7
   def demo = {
+    val move = Move(Pos(3, 2), Pos(1, 0))
     val board1 = Boardie.newBoard(4)
     board1.printBoard()
     val board2 = board1.initialMove(Pos(1, 0))
     board2.printBoard()
-    board2.validateMove(3, 2, 1, 0, printIt = true)
-    val board6 = board2.move(3, 2, 1, 0)
+    board2.validateMove(move, printIt = true)
+    val board6 = board2.move(move)
     board6.printBoard()
     println(board6.allPositions)
   }
   def run() = {
+    val initialMove = Pos(1, 0)
     val startTime = System.nanoTime()
-    val board = Boardie.newBoard(6).initialMove(Pos(0, 0))
+    val board = Boardie.newBoard(size).initialMove(initialMove)
     board.printBoard()
-    solve(board, 0, 65732489, Pos(1,0), startTime)
+    solve(board, 0, bigNumber, initialMove, startTime)
     val endTime = System.nanoTime()
     println(s"Finished in ${(endTime-startTime)/1000.0/1000.0/1000.0}s")
   }
+  val hashmap = mutable.HashMap.empty[Int, Seq[Move]]
+  val useHash = false
   def solve(board: Board, countMoves: Int, record: Int, lastMove: Pos, startTime: Long): Int = {
     if (board.isSolved()) {
+      if (useHash)
+        hashmap.addOne((board.hash, Seq.empty))
       val endTime = System.nanoTime()
       println(s"Found new record of $countMoves in ${(endTime-startTime)/1000.0/1000.0/1000.0}s")
       println(board.moves)
       return countMoves
     }
     if (countMoves >= record) {
-      return 32748
+      return bigNumber
     }
     val moves = board.possibleMoves
     var newRecord = record
@@ -35,9 +44,23 @@ object Main extends App {
       val newBoard = board.move(move)
       //newBoard.printBoard()
       val newMove = if (lastMove.y != move.from.y || lastMove.x != move.from.x) 1 else 0
-      val newVal = if (countMoves+newMove < newRecord) solve(newBoard, countMoves+newMove, newRecord, move.to, startTime) else 34554
+      val newVal = if (countMoves+newMove < newRecord) {
+        if (useHash && hashmap.contains(newBoard.hash)) {
+          //println("Took a shortcut")
+          val shortcutVal = countMoves + newMove + hashmap(newBoard.hash).length
+          if (shortcutVal<newRecord) {
+            val endTime = System.nanoTime()
+            println(s"Found new record of $shortcutVal in ${(endTime-startTime)/1000.0/1000.0/1000.0}s with a shortcut (shortcutlength: ${hashmap(newBoard.hash).length})")
+            println(board.moves++Seq(move)++hashmap(newBoard.hash))
+          }
+          shortcutVal
+        } else
+          solve(newBoard, countMoves+newMove, newRecord, move.to, startTime)
+      } else bigNumber
       if (newVal<newRecord) {
         newRecord = newVal
+        if (useHash)
+          hashmap.addOne((board.hash, Seq(move)++hashmap(newBoard.hash)))
       }
       newVal
     })
