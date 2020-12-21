099c3f142d0207b821fe9667dd85127d119b6463
Johan Särnbratt <johan@minna.tech>
2020-12-21 06:03:16 +0100

    aoc2020 day 20 part 2

--- src/main/scala/aoc2020/Day20.scala
+++ src/main/scala/aoc2020/Day20.scala
@@ -1,47 +1,178 @@
 package aoc2020
 
 object Day20 {
+  def run(): Unit = {
+    val testStartPieces = part1("/Users/johansarnbratt/personal/trianglepeg/src/main/scala/aoc2020/data20test")
+    //val startPiece = part1("/Users/johansarnbratt/personal/trianglepeg/src/main/scala/aoc2020/data20")
+    part2("/Users/johansarnbratt/personal/trianglepeg/src/main/scala/aoc2020/data20test", testStartPieces.head)
+    part2("/Users/johansarnbratt/personal/trianglepeg/src/main/scala/aoc2020/data20test", testStartPieces(1))
+    part2("/Users/johansarnbratt/personal/trianglepeg/src/main/scala/aoc2020/data20test", testStartPieces(2))
+    part2("/Users/johansarnbratt/personal/trianglepeg/src/main/scala/aoc2020/data20test", testStartPieces(3))
+    //part2("/Users/johansarnbratt/personal/trianglepeg/src/main/scala/aoc2020/data20", startPiece)
+  }
 
-  def run() = { //part 2
-    val data: Seq[Seq[String]] = AocHelpers.readDataGroupsSeparatedByBlankLines("/Users/johansarnbratt/personal/trianglepeg/src/main/scala/aoc2020/data20")
-    val edgeNumbers = getEdgeNumbers(data)
-    println(data.length)
-    println(edgeNumbers.length)
-    println(data.length*4-12*11*2)
-    val distinctEdges = edgeNumbers.distinct
-    println(distinctEdges.length)
+  def part1(fileName: String): Seq[Int] = {
+    val data: Seq[Seq[String]] = AocHelpers.readDataGroupsSeparatedByBlankLines(fileName)
+    val edgeNumbers = getMinEdgeNumbers(data)
     val pieceEdgeNumbers: Seq[(Int, Int)] = getPieceEdgeNumbers(data)
     val pieceEdginess = pieceEdgeNumbers.filter {
       case (edge, _) => edgeNumbers.count(_ == edge) < 2
     }.map {
       case (_, piece) => piece
     }
-    println(pieceEdginess)
     val cornerPieces = pieceEdginess.groupBy(x => x).filter {
       case (_, pieces) => pieces.length > 1
     }
+    println(s"part1: ${cornerPieces.keys}")
     println(s"part1: ${cornerPieces.keys.map(_.toLong).product}")
+    cornerPieces.keys.toSeq
   }
-  def getEdgeNumbers(data: Seq[Seq[String]]) = {
-    data.flatMap{
+  def part2(fileName: String, startPieceName: Int): Unit = {
+    val data: Seq[Seq[String]] = AocHelpers.readDataGroupsSeparatedByBlankLines(fileName)
+    val edgeNumbers: Seq[Int] = getMinEdgeNumbers(data)
+    val pieces: Seq[Piece] = data.map {
+      strings =>
+        val pieceNumber = Integer.parseInt(strings.head.slice(5,9))
+        Piece(pieceNumber, strings.tail)
+    }
+    val edgeMap: Seq[(String, (Int, Int))] = pieces.flatMap { piece =>
+      val edges = getEdgesFromPiece(piece.image).zipWithIndex.map {
+        case (edge, edgeInd) => edge -> (piece.name, edgeInd)
+      }
+      val segde = getEdgesFromPiece(piece.mirror().image).zipWithIndex.map {
+        case (edge, edgeInd) => edge -> (piece.name, edgeInd+4)
+      }
+      edges++segde
+    }
+    //val piecesAlongOneEdge: Int = (Math.sqrt(pieces.length)+0.5).toInt
+    //val board: Seq[Seq[Option[Piece]]] = Seq.fill(piecesAlongOneEdge)(Seq.fill(piecesAlongOneEdge)(None))
+    //find orientation of startPiece
+    val startPiece = pieces.find(_.name == startPieceName).get.mirror()
+    val startPieceEdgeNum = getEdgesFromPiece(startPiece.image).map(getMinNumFromEdge)
+    val distinctness = startPieceEdgeNum.map(en => edgeNumbers.count(en == _))
+    //println(distinctness)
+    val rotatedStartPiece = distinctness match {
+      case Seq(1,1,2,2) => startPiece
+      case Seq(2,1,1,2) => startPiece.rotate(1)
+      case Seq(2,2,1,1) => startPiece.rotate(2)
+      case Seq(1,2,2,1) => startPiece.rotate(3)
+    }
+    val firstRow = buildRow(rotatedStartPiece, pieces, edgeMap, 0)
+    println(firstRow.map(_.name))
+    //TODO can build first row of puzzle so far
+    // next step: find first piece in next row, repeat rowbuilding with that piece
+  }
+
+  /**
+   * Starting with startPiece tries to find a matching piece to the right until there is no more pieces
+   */
+  def buildRow(startPiece: Piece, pieces: Seq[Piece], edgeMap: Seq[(String, (Int, Int))], depth: Int): Seq[Piece] = {
+    //println(startPiece.name)
+    //startPiece.pretty()
+    if (depth > 4)
+      Seq(startPiece)
+    else
+      Seq(startPiece) ++ findToRight(startPiece: Piece, pieces, edgeMap).map {
+        piece => buildRow(piece, pieces, edgeMap, depth+1)
+      }.getOrElse(Seq.empty)
+
+  }
+
+  /**
+   * Finds one piece that matches being to the right of startPiece
+   */
+  def findToRight(startPiece: Piece, pieces: Seq[Piece], edgeMap: Seq[(String, (Int, Int))]): Option[Piece] = {
+    val rightEdge: String = getEdgesFromPiece(startPiece.image)(2).reverse
+    /*println()
+    println(rightEdge)
+    println()
+    println(getEdgesFromPiece(startPiece.image))
+    println()*/
+    findMatchingEdge(rightEdge, startPiece.name, edgeMap).map {
+      case (pieceName, orientation) =>
+        val piece = pieces.find(_.name == pieceName).get
+        println(orientation)
+        orientation match {
+          case 0 => piece
+          case 1 => piece.rotate(1)
+          case 2 => piece.rotate(2)
+          case 3 => piece.rotate(3)
+          case 4 => piece.mirror()
+          case 5 => piece.mirror().rotate(1)
+          case 6 => piece.mirror().rotate(2)
+          case 7 => piece.mirror().rotate(3)
+        }
+    }
+  }
+
+  /**
+   * Finds an edge
+   * @param edge
+   * @param notPieceName
+   * @param edgeMap
+   * @return
+   */
+  def findMatchingEdge(edge: String, notPieceName: Int, edgeMap: Seq[(String, (Int, Int))]): Option[(Int, Int)] = {
+    val res = edgeMap.filter {
+      case (innerEdge, (pieceName, _)) => edge == innerEdge && pieceName != notPieceName
+    }
+    if (res.length > 1)
+      println(s"Somethings wrong: $edge $notPieceName $res")
+    res.headOption.map(_._2)
+  }
+  case class Piece(name: Int, image: Seq[String]) {
+    def rotate(quarters: Int): Piece = {
+      val size = image.length
+      quarters match {
+        case 0 => this
+        case 1 =>
+          val newImage = (0 until size).map {
+            ind => image.map(str => str(size-ind-1)).mkString
+          }
+          Piece(name, newImage)
+        case 2 =>
+          Piece(name, image.reverse.map(_.reverse))
+        case 3 =>
+          val newImage = (0 until size).map {
+            ind => image.map(str => str(ind)).mkString.reverse
+          }
+          Piece(name, newImage)
+      }
+    }
+    def mirror(): Piece = Piece(name, image.map(_.reverse))
+    def pretty() = {
+      image.foreach(println(_))
+    }
+  }
+  def getMinEdgeNumbers(data: Seq[Seq[String]]): Seq[Int] = {
+    data.flatMap {
       piece =>
         getEdgesFromPiece(piece.tail).map(getMinNumFromEdge)
     }
   }
-  def getPieceEdgeNumbers(data: Seq[Seq[String]]) = {
+  def getPieceEdgeNumbers(data: Seq[Seq[String]]): Seq[(Int, Int)] = {
     data.flatMap {
       piece =>
         val pieceNumber = Integer.parseInt(piece.head.slice(5,9))
         getEdgesFromPiece(piece.tail).map(getMinNumFromEdge).map(_ -> pieceNumber)
     }
   }
-  def getEdgesFromPiece(piece: Seq[String]) = {
-    Seq(piece.head,
-    piece.last,
-    piece.flatMap((row: String) => row.head.toString).mkString,
-    piece.flatMap((row: String) => row.last.toString).mkString)
+  /*
+  ^ 1--->
+  |     2
+  |     |
+  0     |
+  <---3 v
+   */
+  def getEdgesFromPiece(piece: Seq[String]): Seq[String] = {
+    Seq(
+      piece.flatMap((row: String) => row.head.toString).mkString.reverse,
+      piece.head,
+      piece.flatMap((row: String) => row.last.toString).mkString,
+      piece.last.reverse,
+    )
   }
-  def getMinNumFromEdge(edge: String) = {
+  def getMinNumFromEdge(edge: String): Int = {
     val bin = edge.replace('.','0').replace('#','1')
     val num = Integer.parseInt(bin, 2)
     val numFlipped = Integer.parseInt(bin.reverse, 2)
