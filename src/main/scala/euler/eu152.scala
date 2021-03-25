f584958cf1bb364140557c7b112f87c8c51e97f6
Johan Särnbratt <johan@minna.tech>
2021-03-25 08:47:24 +0100

    eu 152 still very slow

--- src/main/scala/euler/eu152.scala
+++ src/main/scala/euler/eu152.scala
@@ -6,30 +6,24 @@ object eu152 {
   val lcm80sq: BigInt = lcm80*lcm80
   val target = lcm80sq/2
   def run() = {
-    val max = 35
+    val max = 38
     val restSums = (2 to max).map(m => restSum(m, max))
     //println(lcm80sq)
     //restSums.map(println)
     val fs = (2 to max).map(f)
-    var record = lcm80sq
-    def recur(sumSoFar: BigInt, numsSoFar: Seq[Int], n: Int): Unit = {
-      if (abs(sumSoFar - target) < record) {
-        record = abs(sumSoFar - target)
-        println(s"New record $record")
-        println(s"with $numsSoFar")
-      }
+    def recur(sumSoFar: BigInt, numsSoFar: List[Int], n: Int): Unit = {
       if (sumSoFar == target) {
         println(s"Found solution $numsSoFar")
       } else if (n > max || sumSoFar + restSums(n-2) < target) {
       } else if (sumSoFar < target) {
         recur(sumSoFar, numsSoFar, n + 1)
-        recur(sumSoFar + fs(n-2), numsSoFar.appended(n), n + 1)
+        recur(sumSoFar + fs(n-2), n :: numsSoFar, n + 1)
       }
     }
 
     println("Starting...")
     val t = System.nanoTime()
-    recur(0, Seq(), 2)
+    recur(0, Nil, 2)
     println((System.nanoTime()-t)/1000000000.0)
   }
   def abs(int: BigInt) = if (int < 0) -int else int
