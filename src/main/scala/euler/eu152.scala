b7135fd55c8be5747ea5c515552bff244dbed069
Johan Särnbratt <johan@minna.tech>
2021-03-26 22:59:07 +0100

    eu 152 clean

--- src/main/scala/euler/eu152.scala
+++ src/main/scala/euler/eu152.scala
@@ -8,34 +8,25 @@ object eu152 {
   val lcm80sq: BigInt = lcm80*lcm80
   val target = lcm80sq/2
   def run() = {
-    val max = 45
+    val max = 38
     val minVal = f(max)
     val restSums = (2 to max).map(m => restSum(m, max))
     //println(lcm80sq)
     //restSums.map(println)
     val fs = (2 to max).map(f)
-    val memoization = mutable.Map.empty[(BigInt, Int), Int]
-    var usedMemoz = 0
     val calls = mutable.Seq.fill(max+5)(0)
     def recur(sumLeft: BigInt, numsSoFar: List[Int], n: Int): Int = {
       calls(n) = calls(n) + 1
-      if (memoization.contains((sumLeft, n))) {
-        usedMemoz = usedMemoz+1
-        memoization((sumLeft, n))
-      } else if (sumLeft == 0) {
+      if (sumLeft == 0) {
         println(s"Found solution $numsSoFar")
-        //memoization((sumLeft, n)) = 1
         1
       } else if (n > max || sumLeft - restSums(n-2) > 0) {
-        //memoization((sumLeft, n)) = 0
         0
       } else if (sumLeft >= minVal) {
         val res = recur(sumLeft, numsSoFar, n + 1) +
           recur(sumLeft - fs(n-2), n :: numsSoFar, n + 1)
-        //memoization((sumLeft, n)) = res
         res
       } else {
-        //memoization((sumLeft, n)) = 0
         0
       }
     }
@@ -45,7 +36,6 @@ object eu152 {
     val noOfSolutions = recur(target, Nil, 2)
     println((System.nanoTime()-t)/1000000000.0)
     println(s"noOfSolutions: $noOfSolutions")
-    println(s"UsedMemoz: $usedMemoz")
     println(s"calls: ${calls.zipWithIndex}")
     println(s"sum calls: ${calls.sum}")
   }
